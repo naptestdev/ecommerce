cc843a64194620d59c4eb81c6f83e6fee9b546b5
napthedev <phongna.dev@gmail.com>
2022-01-18 08:15:04 +0700

    Fix Axios Authorization header

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "/api",
-  headers: {
-    Authorization: localStorage.getItem("token"),
-  },
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers.Authorization = localStorage.getItem("token");
+  return config;
 });
 
 export default instance;
