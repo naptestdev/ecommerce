3e8209a515f3f2003461b53196924b7f79bc37b4
napthedev <phongna.dev@gmail.com>
2022-01-21 21:54:51 +0700

    Done exporting to xlsx and json

--- client/src/components/ExportDropdown.jsx
+++ client/src/components/ExportDropdown.jsx
@@ -1,13 +1,13 @@
 import axios from "../services/axios";
 
 export default function ExportDropdown({ type }) {
-  const handleExport = async (isExcel) => {
+  const handleExport = async (filetype) => {
     const baseUrl = (await axios.get("export/base-url")).data;
 
     window.open(
-      `${baseUrl}/${type}?token=${localStorage.getItem("admin-token")}${
-        isExcel ? "&excel=1" : ""
-      }`
+      `${baseUrl}/${type}/${filetype}?token=${localStorage.getItem(
+        "admin-token"
+      )}`
     );
   };
 
@@ -22,16 +22,22 @@ export default function ExportDropdown({ type }) {
         className="absolute top-full right-0 flex flex-col items-stretch w-max shadow-md rounded-md overflow-hidden transition-all duration-300 opacity-0 invisible group-focus-within:visible group-focus-within:opacity-100"
       >
         <button
-          onClick={() => handleExport(false)}
+          onClick={() => handleExport("csv")}
           className="text-left bg-white py-2 px-2 hover:brightness-90 transition duration-300"
         >
           Download CSV
         </button>
         <button
-          onClick={() => handleExport(true)}
+          onClick={() => handleExport("xlsx")}
           className="text-left bg-white py-2 px-2 hover:brightness-90 transition duration-300"
         >
           Download XLSX (Excel)
+        </button>
+        <button
+          onClick={() => handleExport("json")}
+          className="text-left bg-white py-2 px-2 hover:brightness-90 transition duration-300"
+        >
+          Download JSON
         </button>
       </div>
     </div>
--- routes/ExportRoute.js
+++ routes/ExportRoute.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+const path = require("path");
+const convertCsvToXlsx = require("@aternus/csv-to-xlsx");
+
 const AuthModel = require("../models/AuthModel");
 const ProductModel = require("../models/ProductModel");
 const OrderModel = require("../models/OrderModel");
@@ -8,107 +12,182 @@ const OrderModel = require("../models/OrderModel");
 const { jsonToCSV } = require("../utils/utils");
 const { verifyDownloadJWT } = require("../controllers/verifyJWT");
 
+const handlers = {
+  users: {
+    formatted: async () => {
+      const allUsers = await AuthModel.find();
+
+      return allUsers.map((user) => ({
+        _id: user._id.toString(),
+        email: user.email,
+        password: user.password,
+        username: user.username,
+        emailVerified: user.emailVerified,
+        "address.fullName": user.address?.fullName || "",
+        "address.phoneNumber": user.address?.phoneNumber || "",
+        "address.city": user.address?.city || "",
+        "address.district": user.address?.district || "",
+        "address.exactAddress": user.address?.exactAddress || "",
+        createdAt: user.createdAt.toISOString(),
+        updatedAt: user.updatedAt.toISOString(),
+      }));
+    },
+    original: async () => {
+      const allUsers = await AuthModel.find();
+      return allUsers.map((user) => ({
+        _id: user._id.toString(),
+        email: user.email,
+        password: user.password,
+        username: user.username,
+        emailVerified: user.emailVerified,
+        address: user.address || {},
+        createdAt: user.createdAt.toISOString(),
+        updatedAt: user.updatedAt.toISOString(),
+      }));
+    },
+  },
+
+  products: {
+    formatted: async () => {
+      const allProducts = await ProductModel.find();
+
+      return allProducts.map((product) => ({
+        _id: product._id.toString(),
+        name: product.name,
+        category: product.category,
+        images: product.images,
+        description: product.description,
+        price: product.price,
+        stock: product.stock,
+        sold: product.sold || 0,
+        ratings: product.ratings,
+        ratingsCount: product.ratingsCount,
+        discount: product.discount,
+        createdAt: product.createdAt.toISOString(),
+        updatedAt: product.updatedAt.toISOString(),
+      }));
+    },
+    original: async () => {
+      const allProducts = await ProductModel.find();
+
+      return allProducts.map((product) => ({
+        _id: product._id.toString(),
+        name: product.name,
+        category: product.category,
+        images: product.images,
+        description: product.description,
+        price: product.price,
+        stock: product.stock,
+        sold: product.sold || 0,
+        ratings: product.ratings,
+        ratingsCount: product.ratingsCount,
+        discount: product.discount,
+        createdAt: product.createdAt.toISOString(),
+        updatedAt: product.updatedAt.toISOString(),
+      }));
+    },
+  },
+
+  orders: {
+    formatted: async () => {
+      const allOrders = await OrderModel.find();
+
+      return allOrders.map((order) => ({
+        _id: order._id.toString(),
+        user: order.user,
+        products: order.products.map((product) => product._id.toString()),
+        amount: order.amount,
+        "address.fullName": order.address.fullName,
+        "address.phoneNumber": order.address.phoneNumber,
+        "address.city": order.address.city,
+        "address.district": order.address.district,
+        "address.exactAddress": order.address.exactAddress,
+        status: order.status,
+        createdAt: order.createdAt.toISOString(),
+        updatedAt: order.updatedAt.toISOString(),
+      }));
+    },
+    original: async () => {
+      const allOrders = await OrderModel.find();
+
+      return allOrders.map((order) => ({
+        _id: order._id.toString(),
+        user: order.user,
+        products: order.products.map((product) => product._id.toString()),
+        amount: order.amount,
+        address: order.address || {},
+        status: order.status,
+        createdAt: order.createdAt.toISOString(),
+        updatedAt: order.updatedAt.toISOString(),
+      }));
+    },
+  },
+};
+
 router.get("/base-url", (req, res) => {
   res.send(`${req.protocol}://${req.get("host")}/api/export`);
 });
 
-router.get("/users", verifyDownloadJWT, async (req, res) => {
+router.get("/:type/:filetype", verifyDownloadJWT, async (req, res) => {
   try {
-    const allUsers = await AuthModel.find();
+    const { type, filetype } = req.params;
 
-    const formatted = allUsers.map((user) => ({
-      id: user._id.toString(),
-      email: user.email,
-      password: user.password,
-      username: user.username,
-      emailVerified: user.emailVerified,
-      "address.fullName": user.address?.fullName || "",
-      "address.phoneNumber": user.address?.phoneNumber || "",
-      "address.city": user.address?.city || "",
-      "address.district": user.address?.district || "",
-      "address.exactAddress": user.address?.exactAddress || "",
-      createdAt: user.createdAt.toISOString(),
-      updatedAt: user.updatedAt.toISOString(),
-    }));
+    if (
+      !["users", "products", "orders"].includes(type) ||
+      !["csv", "xlsx", "json"].includes(filetype)
+    )
+      return res.sendStatus(400);
 
-    const csv = jsonToCSV(formatted);
-
-    res.contentType("csv");
-
-    res.setHeader("Content-disposition", "attachment; filename=users.csv");
-
-    res.send(csv);
-  } catch (error) {
-    console.log(error);
-    if (!res.headersSent) res.sendStatus(500);
-  }
-});
-
-router.get("/products", verifyDownloadJWT, async (req, res) => {
-  try {
-    const allProducts = (await ProductModel.find()).map((item) =>
-      item.toObject()
+    res.contentType(filetype);
+    res.setHeader(
+      "Content-disposition",
+      `attachment; filename=${type}.${filetype}`
     );
 
-    const formatted = allProducts.map((product) => ({
-      id: product._id.toString(),
-      name: product.name,
-      category: product.category,
-      images: product.images,
-      description: product.description,
-      price: product.price,
-      stock: product.stock,
-      sold: product.sold,
-      ratings: product.ratings,
-      ratingsCount: product.ratingsCount,
-      discount: product.discount,
-      createdAt: product.createdAt.toISOString(),
-      updatedAt: product.updatedAt.toISOString(),
-    }));
+    if (filetype === "json") {
+      const json = await handlers[type].original();
+      res.send(JSON.stringify(json, null, 2));
+    } else if (filetype === "csv") {
+      const csv = jsonToCSV(await handlers[type].formatted());
+      res.send(csv);
+    } else if (filetype === "xlsx") {
+      const csv = jsonToCSV(await handlers[type].formatted());
 
-    const csv = jsonToCSV(formatted);
+      fs.mkdirSync("exports", { recursive: true });
 
-    res.contentType("csv");
+      const inputPath = path.resolve(__dirname, "..", "exports", `${type}.csv`);
+      const outputPath = path.resolve(
+        __dirname,
+        "..",
+        "exports",
+        `${type}.xlsx`
+      );
 
-    res.setHeader("Content-disposition", "attachment; filename=products.csv");
+      fs.writeFileSync(inputPath, csv, {
+        encoding: "utf-8",
+      });
 
-    res.send(csv);
+      convertCsvToXlsx(inputPath, outputPath);
+
+      res.sendFile(outputPath);
+    }
   } catch (error) {
     console.log(error);
     if (!res.headersSent) res.sendStatus(500);
   }
 });
 
-router.get("/orders", verifyDownloadJWT, async (req, res) => {
-  try {
-    const allOrders = await OrderModel.find().lean();
-
-    const formatted = allOrders.map((order) => ({
-      id: order._id.toString(),
-      user: order.user,
-      products: order.products.map((product) => product._id.toString()),
-      amount: order.amount,
-      "address.fullName": order.address.fullName,
-      "address.phoneNumber": order.address.phoneNumber,
-      "address.city": order.address.city,
-      "address.district": order.address.district,
-      "address.exactAddress": order.address.exactAddress,
-      status: order.status,
-      createdAt: order.createdAt.toISOString(),
-      updatedAt: order.updatedAt.toISOString(),
-    }));
-
-    const csv = jsonToCSV(formatted);
-
-    res.contentType("csv");
-
-    res.setHeader("Content-disposition", "attachment; filename=orders.csv");
-
-    res.send(csv);
-  } catch (error) {
-    console.log(error);
-    if (!res.headersSent) res.sendStatus(500);
-  }
-});
+// router.get("/orders/:type", verifyDownloadJWT, async (req, res) => {
+//   try {
+//     const formatted = await formatOrders();
+//     const csv = jsonToCSV(formatted);
+//     res.contentType("csv");
+// res.setHeader("Content-disposition", "attachment; filename=orders.csv");
+//     res.send(csv);
+//   } catch (error) {
+//     console.log(error);
+//     if (!res.headersSent) res.sendStatus(500);
+//   }
+// });
 
 module.exports = router;
