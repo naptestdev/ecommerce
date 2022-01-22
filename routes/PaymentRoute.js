5a712199b35c2ab0d9c57a27b26231540b91d2a2
napthedev <phongna.dev@gmail.com>
2022-01-22 23:22:17 +0700

    Momo checkout

--- routes/PaymentRoute.js
+++ routes/PaymentRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { verifyJWT } = require("../controllers/verifyJWT");
 const CartModel = require("../models/CartModel");
 const OrderModel = require("../models/OrderModel");
+const crypto = require("crypto");
+const axios = require("axios").default;
 
 let checkoutSessions = {};
 
@@ -15,31 +16,79 @@ router.post("/create-session", verifyJWT, async (req, res) => {
       user: req.user._id,
     }).populate("products.product");
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      success_url: `${serverBaseUrl}/api/payment/success?sessionId={CHECKOUT_SESSION_ID}`,
-      cancel_url:
-        process.env.NODE_ENV === "production"
-          ? `${serverBaseUrl}/cart`
-          : "http://localhost:3000/cart",
-      line_items: existingCart.products.map((item) => ({
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: item.product.name,
-          },
-          unit_amount: Math.round(
-            (item.product.price - item.product.discount) * 100
-          ),
-        },
-        quantity: item.quantity,
-      })),
-    });
+    if (!existingCart || existingCart.products?.length <= 0)
+      return res.status(400).send({ message: "Cart is empty" });
 
-    checkoutSessions[session.id] = req.user._id;
+    const partnerCode = process.env.MOMO_PARTNER_CODE;
+    const accessKey = process.env.MOMO_ACCESS_KEY;
+    const SECRET_KEY = process.env.MOMO_SECRET_KEY;
+    const requestId = Math.random().toString(36).slice(-8);
+    const orderId = requestId;
+    const amount = `${Math.round(
+      existingCart.products.reduce((prev, item) => {
+        return (
+          prev +
+          Math.round(item.product.price - item.product.discount) * item.quantity
+        );
+      }, 0) * Number(process.env.CONVERSION_UNIT)
+    )}`;
+    const redirectUrl = `${serverBaseUrl}/api/payment/success`;
+    const ipnUrl = "https://google.com";
+    const requestType = "captureWallet";
+    const orderInfo = "Thanh toán đơn hàng eCommerce";
+    const extraData = "";
 
-    res.send(session.url);
+    const raw = {
+      accessKey,
+      amount,
+      extraData,
+      ipnUrl,
+      orderId,
+      orderInfo,
+      partnerCode,
+      redirectUrl,
+      requestId,
+      requestType,
+    };
+
+    const rawSignature = Object.keys(raw)
+      .sort()
+      .reduce((obj, key) => {
+        obj[key] = raw[key];
+        return obj;
+      }, {});
+
+    const signature = crypto
+      .createHmac("sha256", SECRET_KEY)
+      .update(
+        Object.entries(rawSignature)
+          .map(([key, value]) => `${key}=${value}`)
+          .join("&")
+      )
+      .digest("hex");
+
+    axios
+      .post("https://test-payment.momo.vn/v2/gateway/api/create", {
+        partnerCode,
+        requestId,
+        amount,
+        orderId,
+        orderInfo,
+        redirectUrl,
+        ipnUrl,
+        requestType,
+        extraData,
+        lang: "en",
+        signature,
+      })
+      .then((response) => {
+        checkoutSessions[requestId] = req.user._id;
+
+        res.send(response.data.payUrl);
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "Error", error: err.response.data });
+      });
   } catch (error) {
     console.log(error);
     if (!res.headersSent) res.sendStatus(500);
@@ -50,8 +99,15 @@ router.get("/success", async (req, res) => {
   try {
     const serverBaseUrl = `${req.protocol}://${req.get("host")}`;
 
-    if (checkoutSessions[req.query.sessionId]) {
-      const userId = checkoutSessions[req.query.sessionId];
+    if (req.query.resultCode !== "0")
+      return res.redirect(
+        process.env.NODE_ENV === "production"
+          ? `${serverBaseUrl}/cart`
+          : "http://localhost:3000/cart"
+      );
+
+    if (checkoutSessions[req.query.requestId]) {
+      const userId = checkoutSessions[req.query.requestId];
 
       const existingCart = await CartModel.findOne({ user: userId }).populate([
         "products.product",
@@ -78,8 +134,8 @@ router.get("/success", async (req, res) => {
           : "http://localhost:3000/orders"
       );
     } else {
-      res.send({
-        message: "Invalid Session Id",
+      res.status(400).send({
+        message: "Invalid Payment Id",
       });
     }
   } catch (error) {
